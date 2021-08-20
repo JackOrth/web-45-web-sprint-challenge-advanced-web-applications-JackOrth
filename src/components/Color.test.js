b9f506e598bcce6b10f9ce7389c194c6faaf8ec6
JackOrth <jackorth19@gmail.com>
2021-08-20 14:47:34 -0600

    Completed testing.

--- src/components/Color.test.js
+++ src/components/Color.test.js
@@ -5,15 +5,44 @@ import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Color from './Color';
 
+const blankColor = {
+    code: {hex: ""},
+    color: "",
+    id: null,
+}
+const completeColor = {
+    code: {hex: "#dd9a99"},
+    color: "bisque",
+    id: 1,
+}
+
 test("Renders without errors with blank color passed into component", () => {
+    render(<Color color={blankColor}/>)
 });
   
 test("Renders the color passed into component", () => {
+    render(<Color color={completeColor}/>)
+    const color = screen.getByText(/bisque/i)
+    expect(color).toBeInTheDocument();
 });
 
 test("Executes handleDelete and toggleEdit property when the 'x' icon is clicked", () => {
+    const mockDelete = jest.fn();
+    const mockToggle = jest.fn();
+    render(<Color color={blankColor} deleteColor={mockDelete} toggleEdit={mockToggle}/>)
+    const deleteColor = screen.getByText(/x/i)
+    userEvent.click(deleteColor)
+    expect(mockDelete).toHaveBeenCalled()
+    expect(mockToggle).toHaveBeenCalled()
 });
 
 test("Executes setEditColor and toggleEdit property when color div is clicked", () => {
-    
+    const mockSetEdit = jest.fn()
+    const mockToggle = jest.fn()
+    render(<Color color={blankColor} setEditColor={mockSetEdit} toggleEdit={mockToggle}/>)
+    const colors = screen.getByTestId(/color/i)
+    userEvent.click(colors)
+    expect(mockSetEdit).toHaveBeenCalled()
+    expect(mockToggle).toHaveBeenCalled()
+
 });
--- src/components/ColorList.test.js
+++ src/components/ColorList.test.js
@@ -4,11 +4,43 @@ import MutationObserver from 'mutationobserver-shim';
 import { render, screen} from "@testing-library/react";
 import ColorList from './ColorList';
 
+const emptyList = []
+const filledList = [
+    {
+        code: {hex:"#CCFFFF"},
+        color: "Teal",
+        id: 1,
+    },
+    {
+        code: {hex:"#3300ff"},
+        color: "Blue",
+        id: 2, 
+    }
+
+]
 test("Renders an empty list of colors without errors", () => {
+    render(<ColorList colors={emptyList}/>)
 });
 
 test("Renders a list of colors without errors", () => {
+    render(<ColorList colors={filledList}/>)
 });
 
 test("Renders the EditForm when editing = true and does not render EditForm when editing = false", () => {
+    const {rerender} = render(
+        <ColorList colors={filledList} editing={true}/>
+    )
+    const editForm = screen.getByText(/edit color/i)
+    expect(editForm).toBeInTheDocument()
+    const colorLabel = screen.getByText(/color name/i)
+    expect(colorLabel).toBeInTheDocument()
+    const hexLabel = screen.getByText(/hex code/i)
+    expect(hexLabel).toBeInTheDocument()
+
+    rerender(
+        <ColorList colors={filledList} editing={false}/>
+    )
+    expect(editForm).not.toBeInTheDocument()
+    expect(colorLabel).not.toBeInTheDocument()
+    expect(hexLabel).not.toBeInTheDocument()
 });
